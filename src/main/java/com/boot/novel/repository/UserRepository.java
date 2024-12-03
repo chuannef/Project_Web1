aaa51034a31e7c7816ef313eb190f308646d23ed
djksanrs <chinh99562@donga.edu.vn>
2024-12-03 11:13:39 +0700

    findUser by username

--- src/main/java/com/boot/novel/repository/UserRepository.java
+++ src/main/java/com/boot/novel/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.boot.novel.repository;
 
 import com.boot.novel.models.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
-
+    Optional<User> findByUsername(String username);
 }
